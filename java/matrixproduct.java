194c3b167d7f234aa7bff781eb3c44ca9b79b163
zenguedes <72712621+zenguedes@users.noreply.github.com>
2024-02-27 12:06:59 +0000

    Update matrixproduct.java
    added onMultLine

--- java/matrixproduct.java
+++ java/matrixproduct.java
@@ -86,7 +86,51 @@ public class matrixproduct{
     }
     
     public static void OnMultLine(int m_ar, int m_br){
-        System.out.println("todo");
+        long Time1, Time2;
+
+	double temp;
+	int i, j, k;
+
+	double[] pha = new double[m_ar*m_ar];
+	double[] phb = new double[m_ar*m_ar];
+	double[] phc = new double[m_ar*m_ar];
+
+		for(i=0; i<m_ar; i++)
+			for(j=0; j<m_ar; j++)
+				pha[i*m_ar + j] = (double)1.0;
+
+		for(i=0; i<m_br; i++)
+			for(j=0; j<m_br; j++)
+				phb[i*m_br + j] = (double)(i+1);
+		
+		for(i=0; i<m_ar; i++)
+			for(j=0; j<m_ar; j++)
+				phc[i*m_ar + j] = (double)0;
+
+		Time1 = System.currentTimeMillis();
+
+		for(i=0; i<m_ar; i++)
+		{
+			for( k=0; k<m_ar; k++)
+			{	
+				for(j=0; j<m_br; j++)
+				{
+					phc[i*m_ar+j] += pha[i*m_ar+k] * phb[k*m_br+j];
+				}
+			}
+		}
+
+		Time2 = System.currentTimeMillis();
+
+		System.out.printf("Time: %3.3f seconds\n", (double)(Time2-Time1)/1000);
+		
+		System.out.println("Result matrix: ");
+		for (i = 0; i < 1; i++) {
+			for (j = 0; j < Math.min(10, m_br); j++) {
+				System.out.print(phc[j] + " ");
+			}
+		}
+		System.out.println();
     }
 
     public static void OnMultBlock(int m_ar, int m_br, int blockSize){
